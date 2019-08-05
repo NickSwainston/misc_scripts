f0b6c7bec9ac018a2712900aa5c0cd7d101e1a41
NickSwainston <nickaswainston@gmail.com>
2019-08-05 10:52:03 +0800

    Added my cand_move script and update permissions

---
+++ cand_move.py
@@ -0,0 +1,30 @@
+from PIL import Image
+from glob import glob
+import cv2
+import os
+
+for i in range(6):
+    if not os.path.isdir("cand_grade_{0}".format(i)):
+        os.mkdir("cand_grade_{0}".format(i))
+
+
+images = glob('*png')
+print(images[0])
+for i in images:
+    """
+    im = Image.open(i)
+    im.show()
+    cand_grade = input()
+    print(cand_grade)
+    im.close()
+    """
+    print(i)
+    im = cv2.imread(i)
+    cv2.namedWindow('image',cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('image', 1400, 1800)
+    cv2.imshow('image', im)
+    cv2.waitKey(3000)
+    cv2.destroyAllWindows()
+    cand_grade = input()
+    os.rename(i, "cand_grade_{0}/{1}".format(cand_grade, i))
+
