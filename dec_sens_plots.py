69c3f064e7c3055d62b35c56a1a61ca1396caac9
NickSwainston <nickaswainston@gmail.com>
2020-01-13 16:44:33 +0800

    Added an maxium theoretical sensitivty of the MWA plot to dec_sens_plots.py

--- dec_sens_plots.py
+++ dec_sens_plots.py
@@ -68,13 +68,14 @@ if __name__ == "__main__":
         plt.xlim(-90, 65)
         plt.savefig("dec_sens_{}.png".format(freq))
 
-    for freq in [120, 150, 200]:
+    for freq in [120, 150, 185]:
         powers_list = calc_powers(freq)
         sweet_dec_range = [-82.8,-71.4,-63.1,-55.,-47.5,-40.4,-33.5,-26.7,-19.9,-13.,-5.9,1.6,9.7,18.6,29.4,44.8]
         plt.figure(figsize=(15,10))
         for pi, p in enumerate(powers_list):
             #check if they're worth using above 30 deg
-            if max(p[120:]) < 0.05:
+            #if max(p[120:]) < 0.05:
+            if sweet_dec_range[pi] != 44.8:
                 continue
             
             if sweet_dec_range[pi] < -30.:
@@ -89,5 +90,46 @@ if __name__ == "__main__":
         plt.ylim(0.,0.3)
         plt.savefig("dec_sens_zoom_{}.png".format(freq))
 
+    #make a theoretical maximum sensitivty plot 
+    powers_list = calc_powers(185.)
+    max_power = powers_list[0]
+    for pi, sweet_p in enumerate(powers_list):
+        for di, deg_p in enumerate(sweet_p):
+            if max_power[di] < deg_p:
+                max_power[di] = deg_p
 
-   
+    max_sens = 2.5 / np.array(max_power).reshape((178,1))
+    print(max_sens.shape)
+    max_sens_map = np.repeat(max_sens, 360, axis=1)
+    print(max_sens_map)
+
+    #set up plot arrays
+    res = 1
+    map_dec_range = range(-89,89,res)
+    map_ra_range = range(0,361,res)
+    RA=[] ; Dec=[]
+    for i in map_dec_range:
+        for j in map_ra_range:
+            Dec.append(i/180.*np.pi)
+            RA.append(j/180.*np.pi-np.pi)
+    nx=np.array(RA) ; ny=np.array(Dec)
+
+    nx.shape = (len(map_dec_range),len(map_ra_range))
+    ny.shape = (len(map_dec_range),len(map_ra_range))
+
+    import matplotlib.colors as colors
+    fig = plt.figure(figsize=(6, 4))
+    plt.rc("font", size=8)
+    fig.add_subplot(111)
+    ax = plt.axes(projection='mollweide')    
+    plt.pcolor(nx, ny, max_sens_map, cmap='plasma_r', vmin=2., vmax=10.)
+
+    plt.xlabel("Right Ascension")
+    plt.ylabel("Declination")
+    xtick_labels = [ '22h', '20h', '18h', '16h', '14h','12h','10h', '8h', '6h', '4h', '2h']
+    ax.set_xticklabels(xtick_labels, zorder=150)
+    plt.grid(True, color='gray', lw=0.5, linestyle='dotted')
+    plt.colorbar(spacing='uniform', shrink = 0.65, #ticks=[2., 10., 20., 30., 40., 50.], 
+                     label=r"Detection Sensitivity, 10$\sigma$ (mJy)")
+    
+    plt.savefig("max_sense_{}.png".format(185), dpi=1000)
