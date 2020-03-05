e762bed34184006a522df8ca296f5b86b632bd34
NickSwainston <nickaswainston@gmail.com>
2020-03-05 13:14:53 +0800

    Made the ionospheric shift plot script use our signal to noise estimates

--- plot_ionosphere_shift.py
+++ plot_ionosphere_shift.py
@@ -3,17 +3,21 @@ import subprocess
 import os
 from astropy.coordinates import SkyCoord
 import astropy.units as u
-import matplotlib.pyplot as plt
 import re
 
 import os
 import psrqpy
 import pandas
 
+import prof_utils
+from prof_utils import est_sn_from_prof
+
+import matplotlib.pyplot as plt
+
 def plot_sn(obsid, pulsar):
     if pulsar.startswith("J"):
         pulsar = pulsar[1:]
-    raw_grep = []
+    bestprof_files = []
     pointings = os.listdir("/group/mwaops/vcs/{0}/pointings".format(obsid)) 
     #print(pointings)
     for point in pointings:
@@ -21,23 +25,30 @@ def plot_sn(obsid, pulsar):
         file_name = "/group/mwaops/vcs/{0}/pointings/{1}/{0}_{1}_PSR_{2}.pfd.bestprof".format(obsid, point, pulsar)
         #print(file_name)
         if os.path.exists(file_name):
-             with open(file_name, "r") as file:
-                for line in file:
-                     if re.search("sigma", line):
-                         raw_grep.append([point, line])
+            bestprof_files.append([file_name, point])
     #print(raw_grep)
     
     data = []
     max_sn = 0.
-    for d in raw_grep:
-        pointing = d[0]
-        sn = float(d[1].split("~")[-1].split(" sigma")[0])
+    for d in bestprof_files:
+        bestprof_data = prof_utils.get_from_bestprof(d[0])
+        obsid, pulsar, dm, period, period_uncer, obsstart, time_detection,\
+                                       profile, num_bins = bestprof_data
+        print(d)
+        try:
+            sn = est_sn_from_prof(profile, float(period))[0]
+        except:
+            sn = 0
+        if sn is None:
+            sn = 0
         if max_sn < sn:
             max_sn = sn
-        data.append([pointing, sn])
+        data.append([d[1], sn])
     
     #get pulsar position
-    query = psrqpy.QueryATNF(psrs=['J' + pulsar], loadfromdb=os.environ['PSRCAT_FILE']).pandas
+    if not pulsar.startswith("J"):
+        pulsar = "J" + pulsar
+    query = psrqpy.QueryATNF(psrs=[pulsar], loadfromdb=os.environ['PSRCAT_FILE']).pandas
     raj = query["RAJ"][0]
     decj = query["DECJ"][0]
     
@@ -61,7 +72,7 @@ def plot_sn(obsid, pulsar):
             colour = 'black'
         if sn == max_sn:
             colour = 'red'
-        plt.text(ra, dec, str(sn), va='center', ha='center', fontsize=6, color=colour)
+        plt.text(ra, dec, "{:.2f}".format(sn), va='center', ha='center', fontsize=6, color=colour)
     print([min(ras)-0.01, max(ras)+0.01, min(decs)-0.01, max(decs)+0.01])
     plt.axis([min(ras)-0.01, max(ras)+0.01, min(decs)-0.01, max(decs)+0.01])
     plt.xlabel(r"$\Delta$ Right Ascension ($^{\circ}$)")
