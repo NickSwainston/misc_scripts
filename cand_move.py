807bc811d53d16e75f917b85202e477ffb4bb581
Nick Swainston <nickaswainston@gmail.com>
2020-09-07 16:16:36 +0800

    added the option to use matplotlib method if cv2 doesn't work on your machine

--- cand_move.py
+++ cand_move.py
@@ -1,35 +1,50 @@
 #!/usr/bin/env python3
 
-try:
-    from PIL import Image
-except ImportError:
-    import Image
+import argparse
 from glob import glob
-import cv2
 import os
 
+parser = argparse.ArgumentParser(description="""Simple graphical interface for candidate sorting""")
+parser.add_argument("-m", "--method", type=str, default='cv2',
+        help='The method to display the image. Either "matplotlib" or "cv2". Default: cv2.')
+args = parser.parse_args()
+
+if args.method == "cv2":
+    try:
+        from PIL import Image
+    except ImportError:
+        import Image
+    import cv2
+elif args.method == "matplotlib":
+    import matplotlib.pyplot as plt
+    import matplotlib.image as mpimg
+    fig=plt.figure(figsize=(1250, 1000), dpi=1)
+    plt.ion()
+    plt.axis('off')
+    fig.set_tight_layout(True)
+    plt.show()
+else:
+    print("Incorrect method given")
+    exit()
+
+images = glob('*png')
 for i in range(6):
     if not os.path.isdir("cand_grade_{0}".format(i)):
         os.mkdir("cand_grade_{0}".format(i))
 
-
-images = glob('*png')
-print(images[0])
 for i in images:
-    """
-    im = Image.open(i)
-    im.show()
-    cand_grade = input()
-    print(cand_grade)
-    im.close()
-    """
     print(i)
-    im = cv2.imread(i)
-    cv2.namedWindow('image',cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('image', 1400, 1800)
-    cv2.imshow('image', im)
-    cv2.waitKey(2000)
-    cv2.destroyAllWindows()
+    if args.method == "cv2":
+        im = cv2.imread(i)
+        cv2.namedWindow('image',cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('image', 1400, 1800)
+        cv2.imshow('image', im)
+        cv2.waitKey(2000)
+        cv2.destroyAllWindows()
+    elif args.method == "matplotlib":
+        image=mpimg.imread(i)
+        imgplot = plt.imshow(image)
+        plt.show()
     cand_grade = input()
     os.rename(i, "cand_grade_{0}/{1}".format(cand_grade, i))
     pfd = i[:-4]
