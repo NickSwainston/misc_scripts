8cdc3fa92c118a458bf9fc39f13ccbe546b1483e
Nick <nickaswainston@gmail.com>
2021-07-08 11:18:32 +0800

    Completed upload cand scripts

--- upload_cands.py
+++ upload_cands.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 import argparse
-from vcstools.client import upload_beam, upload_obsid
+import glob
+from vcstools.client import upload_beam, upload_obsid, upload_cand
 from vcstools.progress_bar import progress_bar
 
 
@@ -20,17 +21,20 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     print("Uploading obsid")
-    upload_obsid(args.obsid)
+    #upload_obsid(args.obsid)
 
+    pointing_ids = {}
     with open(args.file) as f:
         pointing_list = f.readlines()
         #for p in pointing_list:
         for i, p in enumerate(progress_bar(pointing_list, "Uploading beams")):
             name = "Blind_{}_{}".format(args.obsid, p.strip())
-            upload_beam(name, args.begin, args.end,
-                        mwa_search_command=args.command, mwa_search_version='v3.0', id=i)
-
-    png_files = glob.glob('*png')
-    for pf in png_files:
-        print(pf)
+            pointing_ids[p.strip()] = i + 4
+            #upload_beam(name, args.begin, args.end,
+            #            mwa_search_command=args.command, mwa_search_version='v3.0', supercomputer_id=1)
+    png_files = glob.glob('B*png')
+    for i, pf in enumerate(progress_bar(png_files, "Uploading cands")):
+        pointing = "{}_{}".format(pf.split("_")[2], pf.split("_")[3])
+        #print(pointing_ids[pointing], 'SMART_10min', pf, pf[:-4])
+        upload_cand(pointing_ids[pointing], search_type='SMART_10min', search_params_id=1, png_file=pf, pfd_file=pf[:-4])
         
