ca6dff836cf5ae0079604fe0e838c3a783cb5006
NickSwainston <nickaswainston@gmail.com>
2022-03-05 09:43:52 +0800

    Added new pulsars

--- survey_paper/jname_sort.py
+++ survey_paper/jname_sort.py
@@ -6,10 +6,12 @@ import psrqpy
 import subprocess
 from shutil import copyfile
 import pandas as pd
+import shutil
 
 from vcstools.pointing_utils import format_ra_dec
 from vcstools.metadb_utils import getmeta
-from vcstools.sn_flux_utils import est_pulsar_sn
+from vcstools.radiometer_equation import est_pulsar_sn
+from vcstools.prof_utils import get_from_bestprof
 
 logger = logging.getLogger(__name__)
 
@@ -43,7 +45,7 @@ submit = True
 calid_dict = {'1302282040': 1302171592, '1302540536': 1302603296, '1302712864': 1302732080, '1302106648': 1302085256, '1301847296': 1301826240, '1301674968': 1301739904, '1301412552': 1301480888, '1301240224': 1301221888, '1300981728': 1300962880, '1300809400': 1300790208, '1268321832': 1268321544, '1268063336': 1268063056, '1267459328': 1267459048, '1267283936': 1267268464, '1267111608': 1267111328, '1266680784': 1266664072, '1265470568': 1265470280, '1264867416': 1264850632, '1266932744': 1266932464, '1266329600': 1266329312, '1266155952': 1266155672, '1265725128': 1265724848, '1265983624': 1265983344, '1261241272': 1261240992, '1260638120': 1260637840, '1259427304': 1259427008, '1259685792': 1259691192, '1258221008': 1258219696, '1257617424': 1257622816, '1257010784': 1257010488, '1256407632': 1256381240, '1255803168': 1255899632, '1255197408': 1255208880, '1254594264': 1254593968, '1253991112': 1254000168, '1253471952': 1253477344, '1252780888': 1252705176, '1252177744': 1252100856, '1225462936': 1225478264, '1225118240': 1225133000, '1224859816': 1224874512, '1227009976': 1227007528, '1224252736': 1224277624, '1255444104': 1255443816, '1226062160': 1226054696, '1225713560': 1225710296, '1223042480': 1223068840, '1222697776': 1222695592, '1222435400': 1222434520, '1221832280': 1221831856, '1221399680': 1221342176}
 
 # grabed from the R column of the SMART google sheet
-jname_str = "J2241-5236 J2145-0750 J2222-0137 J2248-0101 J2155-3118 J2048-1616 J2108-3429 J2330-2005 J2325-0530 J0034-0721 J2145-0750 J2336-01 J2234+2114 J2317+2149 J2241-5236 J2330-2005 J0152-1637 J0206-4028 J2354-22 J0134-2937 J0038-2501 J0030+0451 J0034-0721 J0034-0534 J2241-5236 J0133-6957 J2324-6054 J0206-4028 J0255-5304 J0051+0423 J0152-1637 J0151-0635 J0152-1637 J0255-5304 J0206-4028 J0418-4154 J0304+1932 J0450-1248 J0452-1759 J0459-0210 J0401-7608 J0450-1248 J0459-0210 J0452-1759 J0520-2553 J0450-1248 J0514-4408 J0636-4549 J0702-4956 J0600-5756 J0437-4715 J0630-2834 J0737-3039A J0636-4549 J0534+2200 J0528+2200 J0450-1248 J0452-1759 J0459-0210 J0601-0527 J0624-0424 J0630-2834 J0820-1350 J0737-3039A J0742-2822 J0729-1836 J0742-2822 J0749-4247 J0820-4114 J0820-3921 J0835-4510 J0837-4135 J0838-3947 J0924-5302 J0942-5552 J0955-5304 J0959-4809 J0826+2637 J0837+0610 J0922+0638 J0729-1448 J0729-1836 J0742-2822 J0758-1528 J0820-1350 J0930-2301 J0837-4135 J0856-6137 J0905-6019 J0924-5302 J0924-5814 J0942-5657 J0842-4851 J0907-5157 J0902-6325 J0856-6137 J0902-6325 J0904-7459 J0905-6019 J0924-5302 J0924-5814 J0942-5552 J0942-5657 J0922+0638 J1022+1001 J0953+0755 J0908-1739 J0820-4114 J0742-2822 J0820-1350 J0835-4510 J0837-4135 J0855-3331 J1012-2337 J0835-4510 J0837-4135 J0924-5302 J0942-5657 J0955-5304 J0959-4809 J1003-4747 J1057-5226 J1116-4122 J0922+0638 J0943+1631 J0946+0951 J0953+0755 J0908-1739 J0944-1354 J1018-1642 J1057-5226 J1059-5742 J1116-4122 J1121-5444 J1123-4844 J1123-6651 J1136-5525 J1141-6545 J1146-6030 J1202-5820 J1224-6407 J1225-5556 J1240-4124 J1312-5402 J1320-5359 J0953+0755 J1136+1551 J1012-2337 J1018-1642 J1034-3224 J1041-1942 J1311-1228 J1257-1027 J1313+0931 J1300+1240 J1059-5742 J1202-5820 J1224-6407 J1430-6623 J1141-6545 J1112-6926 J1239-6832 J1456-6843 J1340-6456 J1123-6651  J1328-4357 J1240-4124 J1355-5153 J1418-3921 J1335-3642 J1320-5359 J1311-1228 J1332-3032 J1418-3921 J1313+0931 J1300+1240 J1311-1228 J1453-6413 J1456-6843 J1430-6623 J1534-5334 J1440-6344 J1355-5153 J1418-3921 J1543-0620 J1543+0929 J1510-4422 J1527-3931 J1455-3330 J1507-4352 J1418-3921 J1534-5334 J1536-4948"
+jname_str = "J0823+0159 J0614+2229 J0525+1115 J2241-5236 J2145-0750 J2222-0137 J2248-0101 J2155-3118 J2048-1616 J2108-3429 J2330-2005 J2325-0530 J0034-0721 J2145-0750 J2336-01 J2234+2114 J2317+2149 J2241-5236 J2330-2005 J0152-1637 J0206-4028 J2354-22 J0134-2937 J0038-2501 J0030+0451 J0034-0721 J0034-0534 J2241-5236 J0133-6957 J2324-6054 J0206-4028 J0255-5304 J0051+0423 J0152-1637 J0151-0635 J0152-1637 J0255-5304 J0206-4028 J0418-4154 J0304+1932 J0450-1248 J0452-1759 J0459-0210 J0401-7608 J0450-1248 J0459-0210 J0452-1759 J0520-2553 J0450-1248 J0514-4408 J0636-4549 J0702-4956 J0600-5756 J0437-4715 J0630-2834 J0737-3039A J0636-4549 J0534+2200 J0528+2200 J0450-1248 J0452-1759 J0459-0210 J0601-0527 J0624-0424 J0630-2834 J0820-1350 J0737-3039A J0742-2822 J0729-1836 J0742-2822 J0749-4247 J0820-4114 J0820-3921 J0835-4510 J0837-4135 J0838-3947 J0924-5302 J0942-5552 J0955-5304 J0959-4809 J0826+2637 J0837+0610 J0922+0638 J0729-1448 J0729-1836 J0742-2822 J0758-1528 J0820-1350 J0930-2301 J0837-4135 J0856-6137 J0905-6019 J0924-5302 J0924-5814 J0942-5657 J0842-4851 J0907-5157 J0902-6325 J0856-6137 J0902-6325 J0904-7459 J0905-6019 J0924-5302 J0924-5814 J0942-5552 J0942-5657 J0922+0638 J1022+1001 J0953+0755 J0908-1739 J0820-4114 J0742-2822 J0820-1350 J0835-4510 J0837-4135 J0855-3331 J1012-2337 J0835-4510 J0837-4135 J0924-5302 J0942-5657 J0955-5304 J0959-4809 J1003-4747 J1057-5226 J1116-4122 J0922+0638 J0943+1631 J0946+0951 J0953+0755 J0908-1739 J0944-1354 J1018-1642 J1057-5226 J1059-5742 J1116-4122 J1121-5444 J1123-4844 J1123-6651 J1136-5525 J1141-6545 J1146-6030 J1202-5820 J1224-6407 J1225-5556 J1240-4124 J1312-5402 J1320-5359 J0953+0755 J1136+1551 J1012-2337 J1018-1642 J1034-3224 J1041-1942 J1311-1228 J1257-1027 J1313+0931 J1300+1240 J1059-5742 J1202-5820 J1224-6407 J1430-6623 J1141-6545 J1112-6926 J1239-6832 J1456-6843 J1340-6456 J1123-6651  J1328-4357 J1240-4124 J1355-5153 J1418-3921 J1335-3642 J1320-5359 J1311-1228 J1332-3032 J1418-3921 J1313+0931 J1300+1240 J1311-1228 J1453-6413 J1456-6843 J1430-6623 J1534-5334 J1440-6344 J1355-5153 J1418-3921 J1543-0620 J1543+0929 J1510-4422 J1527-3931 J1455-3330 J1507-4352 J1418-3921 J1534-5334 J1536-4948"
 jname_str = jname_str.replace("  ", " ")
 jname_list = list(dict.fromkeys(jname_str.split(" ")))
 jname_dict = {}
@@ -76,8 +78,15 @@ df = pd.DataFrame(columns=["Pulsar",
                            "SN",
                            "SN Uncertainty",
                            "Predicted SN",
-                           "Predicted SN Uncertainty"])
-query = psrqpy.QueryATNF(params = ['PSRJ', 'DM', 'P0', 'RAJ', 'DECJ']).pandas
+                           "Predicted SN Uncertainty",
+                           "Searched Bool",
+                           "Search offset (deg)",
+                           "Search Bestprof location",
+                           "Search Bestprof sigma",
+                           "Search SN",
+                           "Search SN Uncertainty",
+                           ])
+query = psrqpy.QueryATNF().pandas
 #with open('SMART_pulsars.csv', 'w', newline='') as csvfile:
 #    spamwriter = csv.writer(csvfile)
 for jname, obsids in sorted(jname_dict.items()):
@@ -86,7 +95,8 @@ for jname, obsids in sorted(jname_dict.items()):
         if o == 0:
             joutput = jname
         else:
-            joutput = ""
+            #joutput = ""
+            joutput = jname
         query_id = list(query['PSRJ']).index(jname)
         # See if dpp ran and what is the best bin size
         #print("/astro/mwavcs/vcs/{0}/dpp/{0}_{1}/{0}_{1}*bins_gaussian_fit.png".format(
@@ -116,7 +126,7 @@ for jname, obsids in sorted(jname_dict.items()):
                 bins = int(min(all_bins))
         else:
             bins = ""
-            
+
         # Find files, always look in SMART dir first
 
         # Find bestprof
@@ -142,8 +152,8 @@ for jname, obsids in sorted(jname_dict.items()):
                                 obsid, jname, bins))
             if len(dpp_png) != 0:
                 png = dpp_png[0]
-    
-        # Find archive 
+
+        # Find archive
         dpp_archive = glob.glob("/astro/mwavcs/vcs/{0}/dpp/{0}_{1}/{0}_{1}_archive.ar".format(
                                 obsid, jname))
         if len(dpp_archive) != 0:
@@ -202,15 +212,17 @@ for jname, obsids in sorted(jname_dict.items()):
             """
         # Grab DM and SN
         if bestprof:
+            _, _, dm, _, _, _, best_beg, best_dur, _, _, _ = get_from_bestprof(bestprof, pointing_input="{}_{}".format(query["RAJ"][query_id], query["DECJ"][query_id]))
+            best_end = best_beg + best_dur - 1
             with open(bestprof, "r") as bestprof_f:
                 lines = bestprof_f.readlines()
-                dm = lines[14][22:-1]
+            #    dm = lines[14][22:-1]
                 sigma = lines[13].split("(~")[-1].split(" sigma")[0]
         else:
             dm = None
             sigma = None
 
-        
+
         #spamwriter.writerow([joutput, obsid, query["P0"][query_id], query["DM"][query_id], dm, sigma, bestprof, png, archive])
         flux = u_flux = sn = u_sn = SN = SN_err = None
         if not png or not bestprof:
@@ -219,19 +231,24 @@ for jname, obsids in sorted(jname_dict.items()):
         elif submit:
             ra_dec_list = format_ra_dec([[query["RAJ"][query_id], query["DECJ"][query_id]]])
             pointing = "{}_{}".format(ra_dec_list[0][0], ra_dec_list[0][1])
-            #print("/astro/mwavcs/vcs/{0}/sefd_simulations/{1}_{0}_*.stats".format(obsid, jname))
+
+            # Remove files for resubmission
+            # sefd_loc = glob.glob("/astro/mwavcs/vcs/{0}/sefd_simulations/{1}_{0}_*.stats".format(obsid, jname))
+            # for sefd in sefd_loc:
+            #     os.remove(sefd)
             sefd_loc = glob.glob("/astro/mwavcs/vcs/{0}/sefd_simulations/{1}_{0}_*.stats".format(obsid, jname))
             if len(sefd_loc) == 0:
                 print("Missing sefd for {} {} so resubmitting".format(obsid, jname))
                 command = "submit_to_database.py -o {} -O {} -b {} -p {} --pointing {} --vcstools_version nswainston --dont_upload".format(obsid, calid_dict[obsid], bestprof, jname, pointing)
-                print(command)
+
                 #test = subprocess.Popen(command.split(" "), stdout=subprocess.PIPE)
                 #output = test.communicate()[0]
                 os.system(command)
             else:
-                #os.remove(sefd_loc[0])
+                #shutil.copy(sefd_loc[0], "/astro/mwavcs/pulsar_search/SMART_quick_look_detection/")
                 flux_loc = glob.glob("/astro/mwavcs/pulsar_search/SMART_quick_look_detection/{0}_{1}_*_flux_results.csv".format(jname, obsid))
                 if len(flux_loc) == 0:
+                #if True:
                     print("No flux calc result, rerunning")
                     command = "submit_to_database.py -o {} -O {} -b {} -p {} --pointing {} --vcstools_version nswainston --dont_upload --sefd_file {}".format(obsid, calid_dict[obsid], bestprof, jname, pointing, sefd_loc[0])
                     print(command)
@@ -250,11 +267,13 @@ for jname, obsids in sorted(jname_dict.items()):
                             elif row[0] == 'u_sn':
                                 u_sn = row[1]
                     # Est SN
-                    full_metadata = getmeta(service='obs', params={'obs_id':obsid})
-                    beg = full_metadata[u'stoptime'] 
-                    end = full_metadata[u'starttime']
-                    SN, SN_err, s_mean, s_mean_err = est_pulsar_sn(jname, obsid, beg, end,
-                                  full_meta=full_metadata)
+                    #full_metadata = getmeta(service='obs', params={'obs_id':obsid})
+                    #beg = full_metadata[u'stoptime']
+                    #end = full_metadata[u'starttime']
+                    # SN, SN_err, s_mean, s_mean_err = est_pulsar_sn(jname, obsid,
+                    #                                                dect_beg=best_beg, dect_end=best_end,
+                    #                                                query=query)
+                    # print("Sn pred results:", SN, SN_err, s_mean, s_mean_err)
             # move files to saved dir
             """
             dst = "/astro/mwavcs/pulsar_search/SMART_quick_look_detection/{}".format(obsid)
@@ -271,7 +290,7 @@ for jname, obsids in sorted(jname_dict.items()):
         # output results
         df = df.append({"Pulsar":joutput,
                         "ObservationID":obsid,
-                        "ATNF Period (s)":query["P0"][query_id], 
+                        "ATNF Period (s)":query["P0"][query_id],
                         "ATNF DM":query["DM"][query_id],
                         "Bextprof DM":dm,
                         "Bestprof sigma":sigma,
