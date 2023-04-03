8931e1ea6a4c9c539033839207f0b4c44dd4b68b
NickSwainston <nickaswainston@gmail.com>
2023-04-03 12:16:49 +0800

    Started making changed to the antf comparison to use bandwidth intergration

--- spectra_paper/antf_comparison_plot.py
+++ spectra_paper/antf_comparison_plot.py
@@ -3,18 +3,17 @@ from pulsar_spectra.spectral_fit import find_best_spectral_fit
 import psrqpy
 import matplotlib.pyplot as plt
 
-query = psrqpy.QueryATNF(loadfromdb=catalogue.ATNF_LOC).pandas
 #pulsars = ['J0820-1350', 'J0835-4510', 'J1820-0427']
-pulsars = ['J0835-4510', 'J1141-6545', 'J1751-4657', 'J0953+0755']
-#pulsars = ['J0835-4510', 'J1751-4657', 'J0953+0755', 'J1607-0032']
+#pulsars = ['J0835-4510', 'J0953+0755']
+pulsars = ['J0835-4510', 'J1751-4657', 'J0953+0755', 'J1607-0032']
 np = len(pulsars)
 
 fig, axs = plt.subplots(nrows=np, ncols=2, figsize=(10, 4*np))
 
-antf_dict = catalogue.all_flux_from_atnf(query=query)
+antf_dict = catalogue.all_flux_from_atnf()
 # Resort into cat_list format
 for pi, pulsar in enumerate(pulsars):
-    antf_cat = { pulsar : [ [], [], [], [] ] }
+    antf_cat = { pulsar : [ [], [], [], [], [] ] }
     for ref in antf_dict[pulsar].keys():
         if ref == "Mignani_2017_ATNF":
             continue
@@ -23,18 +22,20 @@ for pi, pulsar in enumerate(pulsars):
                                         antf_dict[pulsar][ref]['Flux Density mJy'],
                                         antf_dict[pulsar][ref]['Flux Density error mJy']):
             antf_cat[pulsar][0] += [freq]
-            antf_cat[pulsar][1] += [flux]
-            antf_cat[pulsar][2] += [flux_err]
-            antf_cat[pulsar][3] += [ref]
-    freqs, fluxs, flux_errs, refs = antf_cat[pulsar]
-    model, m, fit_info, p_best, p_category = find_best_spectral_fit(pulsar, freqs, fluxs, flux_errs, refs, plot_best=True, alternate_style=True, axis=axs[pi][0])
+            antf_cat[pulsar][1] += [None]
+            antf_cat[pulsar][2] += [flux]
+            antf_cat[pulsar][3] += [flux_err]
+            antf_cat[pulsar][4] += [ref]
+    freqs, bands, fluxs, flux_errs, refs = antf_cat[pulsar]
+    model, m, fit_info, p_best, p_category = find_best_spectral_fit(pulsar, freqs, bands, fluxs, flux_errs, refs, plot_best=True, alternate_style=True, axis=axs[pi][0])
     axs[pi][0].set_title(f'PSR {pulsar} ATNF')
 
 
-    cat_dict = catalogue.collect_catalogue_fluxes(query=query, exclude=["Taylor_1993"])
+    cat_dict = catalogue.collect_catalogue_fluxes(exclude=["Taylor_1993"])
     # reorganise so it is in the same order
-    freqs, fluxs, flux_errs, refs = cat_dict[pulsar]
+    freqs, bands, fluxs, flux_errs, refs = cat_dict[pulsar]
     fo = []
+    bo = []
     flo = []
     fleo = []
     ro = []
@@ -46,16 +47,17 @@ for pi, pulsar in enumerate(pulsars):
     #             fleo.append(flei)
     #             ro.append(ri)
     # put the rest in
-    for fi, fli, flei, ri in zip(freqs, fluxs, flux_errs, refs):
+    for fi, bi, fli, flei, ri in zip(freqs, bands, fluxs, flux_errs, refs):
         if ri not in antf_dict[pulsar].keys():
             fo.append(fi)
+            bo.append(bi)
             flo.append(fli)
             fleo.append(flei)
             ro.append(ri)
 
-    for freq, flux, flux_err, ref in zip(fo, flo, fleo, ro):
-        print(f"{str(freq):8s}{float(flux):8.2f}{float(flux_err):8.2f} {str(ref):20s}")
-    model, m, fit_info, p_best, p_category = find_best_spectral_fit(pulsar, fo, flo, fleo, ro, plot_best=True, alternate_style=True, axis=axs[pi][1])
+    for freq, band, flux, flux_err, ref in zip(fo, bo, flo, fleo, ro):
+        print(f"{str(freq):8s}{str(band):8s}{float(flux):8.2f}{float(flux_err):8.2f} {str(ref):20s}")
+    model, m, fit_info, p_best, p_category = find_best_spectral_fit(pulsar, fo, bo, flo, fleo, ro, plot_best=True, alternate_style=True, axis=axs[pi][1])
     axs[pi][1].set_title(f'PSR {pulsar} pulsar_spectra')
     print(model)
 
