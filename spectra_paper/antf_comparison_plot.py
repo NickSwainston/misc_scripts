b363ded0807b62a0ff05bb0b148ede7da2cd7951
NickSwainston <nickaswainston@gmail.com>
2023-04-04 17:13:39 +0800

    Use same x axis and ref order for antf comparison

--- spectra_paper/antf_comparison_plot.py
+++ spectra_paper/antf_comparison_plot.py
@@ -1,11 +1,10 @@
 from pulsar_spectra import catalogue
 from pulsar_spectra.spectral_fit import find_best_spectral_fit
-import psrqpy
 import matplotlib.pyplot as plt
 
 #pulsars = ['J0820-1350', 'J0835-4510', 'J1820-0427']
 #pulsars = ['J0835-4510', 'J0953+0755']
-pulsars = ['J0835-4510', 'J1751-4657', 'J0953+0755', 'J1607-0032']
+pulsars = ['J0543+2329', 'J1932+1059', 'J1835-0643', 'J0837+0610']
 np = len(pulsars)
 
 fig, axs = plt.subplots(nrows=np, ncols=2, figsize=(10, 4*np))
@@ -13,10 +12,12 @@ fig, axs = plt.subplots(nrows=np, ncols=2, figsize=(10, 4*np))
 antf_dict = catalogue.all_flux_from_atnf()
 # Resort into cat_list format
 for pi, pulsar in enumerate(pulsars):
+    antf_refs = []
     antf_cat = { pulsar : [ [], [], [], [], [] ] }
     for ref in antf_dict[pulsar].keys():
-        if ref == "Mignani_2017_ATNF":
+        if ref in ("Mignani_2017_ATNF", "Taylor_1993_ATNF"):
             continue
+        antf_refs.append(ref[:-5])
         # Update list
         for freq, flux, flux_err in zip(antf_dict[pulsar][ref]['Frequency MHz'],
                                         antf_dict[pulsar][ref]['Flux Density mJy'],
@@ -31,7 +32,7 @@ for pi, pulsar in enumerate(pulsars):
     axs[pi][0].set_title(f'PSR {pulsar} ATNF')
 
 
-    cat_dict = catalogue.collect_catalogue_fluxes(exclude=["Taylor_1993"])
+    cat_dict = catalogue.collect_catalogue_fluxes()
     # reorganise so it is in the same order
     freqs, bands, fluxs, flux_errs, refs = cat_dict[pulsar]
     fo = []
@@ -39,16 +40,19 @@ for pi, pulsar in enumerate(pulsars):
     flo = []
     fleo = []
     ro = []
-    # for ref in antf_dict[pulsar].keys():
-    #     for fi, fli, flei, ri in zip(freqs, fluxs, flux_errs, refs):
-    #         if ref[:-5] == ri:
-    #             fo.append(fi)
-    #             flo.append(fli)
-    #             fleo.append(flei)
-    #             ro.append(ri)
+    for ref in antf_refs:
+        for fi, bi, fli, flei, ri in zip(freqs, bands, fluxs, flux_errs, refs):
+            if ref == ri:
+                fo.append(fi)
+                bo.append(bi)
+                flo.append(fli)
+                fleo.append(flei)
+                ro.append(ri)
     # put the rest in
+    print(refs)
+    print(antf_refs)
     for fi, bi, fli, flei, ri in zip(freqs, bands, fluxs, flux_errs, refs):
-        if ri not in antf_dict[pulsar].keys():
+        if ri not in antf_refs:
             fo.append(fi)
             bo.append(bi)
             flo.append(fli)
@@ -67,6 +71,12 @@ for pi, pulsar in enumerate(pulsars):
     ymax = max(flo + fluxs) ** 1.1
     axs[pi][0].set_ylim(ymin, ymax)
     axs[pi][1].set_ylim(ymin, ymax)
+    #make same x axis the same
+    print(fo)
+    xmin = min(fo) * 0.7
+    xmax = max(fo) * 1.5
+    axs[pi][0].set_xlim(xmin, xmax)
+    axs[pi][1].set_xlim(xmin, xmax)
 #axs[ax_i//cols, ax_i%cols].set_title('PSR '+pulsar)
 
 plt.tight_layout(pad=2.5)
